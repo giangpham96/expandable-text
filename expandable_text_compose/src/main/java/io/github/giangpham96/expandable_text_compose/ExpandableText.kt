76111f7ba6ebd98471cae1a00e1f1162cb3a3353
Giang Pham <giangp2901@gmail.com>
2023-05-08 18:13:30 +0300

    Use text measurer to make code more readable

--- expandable_text_compose/src/main/java/io/github/giangpham96/expandable_text_compose/ExpandableText.kt
+++ expandable_text_compose/src/main/java/io/github/giangpham96/expandable_text_compose/ExpandableText.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.LocalContentColor
@@ -21,14 +22,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.MeasureResult
-import androidx.compose.ui.layout.Placeable
-import androidx.compose.ui.layout.SubcomposeLayout
-import androidx.compose.ui.layout.SubcomposeMeasureScope
-import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.Paragraph
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextLayoutResult
@@ -37,12 +34,12 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.text.style.ResolvedTextDirection.Ltr
 import androidx.compose.ui.text.style.ResolvedTextDirection.Rtl
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Constraints
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
@@ -71,6 +68,7 @@ import androidx.compose.ui.unit.dp
  * text will be positioned as if there was unlimited horizontal space. If [softWrap] is false,
  * @param style Style configuration for the text such as color, font, line height etc.
  */
+@OptIn(ExperimentalTextApi::class)
 @Composable
 fun ExpandableText(
     originalText: String,
@@ -91,147 +89,107 @@ fun ExpandableText(
     style: TextStyle = LocalTextStyle.current,
     animationSpec: AnimationSpec<Float> = spring(),
 ) {
-    val animatableHeight = remember { Animatable(0f) }
-    var collapsedText by remember { mutableStateOf(AnnotatedString("")) }
-    var collapsedHeight by remember { mutableStateOf(0f) }
-    var expandedHeight by remember { mutableStateOf(0f) }
-    // internalExpand == expand means it's the first composition, thus, no animation needed
-    var internalExpand by remember { mutableStateOf(expand) }
-    var displayedText by remember { mutableStateOf(AnnotatedString(originalText)) }
-    var displayedLines by remember { mutableStateOf(limitedMaxLines) }
-    var invalidate by remember { mutableStateOf(true) }
-    LaunchedEffect(expand, collapsedHeight, expandedHeight, collapsedText) {
-        if (internalExpand != expand) {
-            displayedText = AnnotatedString(originalText)
-            displayedLines = Int.MAX_VALUE
-            animatableHeight.animateTo(
-                targetValue = if (expand) expandedHeight else collapsedHeight,
-                animationSpec = animationSpec
-            )
-            internalExpand = expand
-        } else {
-            animatableHeight.snapTo(targetValue = if (expand) expandedHeight else collapsedHeight)
-        }
-        displayedText = if (expand) AnnotatedString(originalText) else collapsedText
-        displayedLines = if (expand) Int.MAX_VALUE else limitedMaxLines
-    }
-    LaunchedEffect(
-        originalText,
-        expandAction,
-        fontSize,
-        fontStyle,
-        fontFamily,
-        letterSpacing,
-        textDecoration,
-        style,
-        modifier,
-        limitedMaxLines,
-    ) {
-        invalidate = true
-    }
-
-    SubcomposeLayout(
-        modifier = modifier
-    ) { cons ->
-        if (invalidate) {
-            val heights = measureExpandableText(
-                expandAction = {
-                    Text(
-                        text = "… $expandAction",
-                        color = color,
-                        fontSize = fontSize,
-                        fontStyle = fontStyle,
-                        fontWeight = fontWeight,
-                        fontFamily = fontFamily,
-                        letterSpacing = letterSpacing,
-                        textDecoration = textDecoration,
-                        lineHeight = lineHeight,
-                        maxLines = 1,
-                        softWrap = softWrap,
-                        style = style,
-                        modifier = it
-                    )
-                },
-                collapsedText = { mod, expandActionPlaceable ->
-                    Text(
-                        text = originalText,
-                        color = color,
-                        fontSize = fontSize,
-                        fontStyle = fontStyle,
-                        fontWeight = fontWeight,
-                        fontFamily = fontFamily,
-                        letterSpacing = letterSpacing,
-                        textDecoration = textDecoration,
-                        lineHeight = lineHeight,
-                        maxLines = limitedMaxLines,
-                        softWrap = softWrap,
-                        style = style,
-                        onTextLayout = { result ->
-                            collapsedText = result.resolveCollapsedText(
-                                originalText = originalText,
-                                expandAction = expandAction,
-                                expandActionWidth = expandActionPlaceable.width,
-                                expandActionColor = expandActionColor
-                            )
-                        },
-                        modifier = mod,
-                    )
-                },
-                expandedText = { mod, _ ->
-                    Text(
-                        text = originalText,
-                        color = color,
-                        fontSize = fontSize,
-                        fontStyle = fontStyle,
-                        fontWeight = fontWeight,
-                        fontFamily = fontFamily,
-                        letterSpacing = letterSpacing,
-                        textDecoration = textDecoration,
-                        lineHeight = lineHeight,
-                        softWrap = softWrap,
-                        style = style,
-                        modifier = mod,
-                    )
-                },
-                constraints = cons,
-            )
-            collapsedHeight = heights.collapsedHeight
-            expandedHeight = heights.expandedHeight
-            invalidate = false
-        }
-        layoutExpandableText(constraints = cons) { mod ->
-            Text(
-                text = if (expand == internalExpand) {
-                    if (expand) AnnotatedString(originalText) else collapsedText
-                } else {
-                    displayedText
-                },
-                modifier = mod.height(
-                    with(LocalDensity.current) {
-                        if (expand == internalExpand) {
-                            if (expand) expandedHeight.toDp() else collapsedHeight.toDp()
-                        } else {
-                            animatableHeight.value.toDp()
-                        }
-                    }
-                ),
+    BoxWithConstraints(modifier) {
+        val mergedStyle = style.merge(
+            TextStyle(
                 color = color,
                 fontSize = fontSize,
-                fontStyle = fontStyle,
                 fontWeight = fontWeight,
-                fontFamily = fontFamily,
-                letterSpacing = letterSpacing,
-                textDecoration = textDecoration,
                 lineHeight = lineHeight,
+                fontFamily = fontFamily,
+                textDecoration = textDecoration,
+                fontStyle = fontStyle,
+                letterSpacing = letterSpacing
+            )
+        )
+        val animatableHeight = remember { Animatable(0f) }
+        // internalExpand == expand means it's the first composition, thus, no animation needed
+        var internalExpand by remember { mutableStateOf(expand) }
+        var displayedText by remember { mutableStateOf(AnnotatedString(originalText)) }
+        var displayedLines by remember { mutableStateOf(limitedMaxLines) }
+        val textMeasurer = rememberTextMeasurer()
+        val expandActionWidth = remember(expandAction, mergedStyle, softWrap) {
+            textMeasurer.measure(
+                text = AnnotatedString("… $expandAction"),
+                style = mergedStyle,
                 softWrap = softWrap,
-                style = style,
-                maxLines = if (expand == internalExpand) {
-                    if (expand) Int.MAX_VALUE else limitedMaxLines
-                } else {
-                    displayedLines
-                },
+                maxLines = 1,
+            ).size.width
+        }
+        val collapsedLayoutResult = remember(originalText, mergedStyle, softWrap) {
+            textMeasurer.measure(
+                text = AnnotatedString(originalText),
+                style = mergedStyle,
+                constraints = constraints,
+                softWrap = softWrap,
+                maxLines = limitedMaxLines,
             )
         }
+        val collapsedHeight = collapsedLayoutResult.size.height
+        val expandedHeight = remember(originalText, mergedStyle, softWrap) {
+            textMeasurer.measure(
+                text = AnnotatedString(originalText),
+                style = mergedStyle,
+                softWrap = softWrap,
+                constraints = constraints,
+            ).size.height
+        }
+        val collapsedText = remember(collapsedLayoutResult) {
+            collapsedLayoutResult.resolveCollapsedText(
+                originalText,
+                expandAction,
+                expandActionWidth,
+                expandActionColor,
+            )
+        }
+        LaunchedEffect(expand, collapsedHeight, expandedHeight, collapsedText) {
+            if (internalExpand != expand) {
+                displayedText = AnnotatedString(originalText)
+                displayedLines = Int.MAX_VALUE
+                animatableHeight.animateTo(
+                    targetValue = (if (expand) expandedHeight else collapsedHeight).toFloat(),
+                    animationSpec = animationSpec
+                )
+                internalExpand = expand
+            } else {
+                animatableHeight.snapTo(
+                    targetValue = (if (expand) expandedHeight else collapsedHeight).toFloat(),
+                )
+            }
+            displayedText = if (expand) AnnotatedString(originalText) else collapsedText
+            displayedLines = if (expand) Int.MAX_VALUE else limitedMaxLines
+        }
+        Text(
+            text = if (expand == internalExpand) {
+                if (expand) AnnotatedString(originalText) else collapsedText
+            } else {
+                displayedText
+            },
+            modifier = Modifier.height(
+                with(LocalDensity.current) {
+                    if (expand == internalExpand) {
+                        if (expand) expandedHeight.toDp() else collapsedHeight.toDp()
+                    } else {
+                        animatableHeight.value.toDp()
+                    }
+                }
+            ),
+            color = color,
+            fontSize = fontSize,
+            fontStyle = fontStyle,
+            fontWeight = fontWeight,
+            fontFamily = fontFamily,
+            letterSpacing = letterSpacing,
+            textDecoration = textDecoration,
+            lineHeight = lineHeight,
+            softWrap = softWrap,
+            style = style,
+            maxLines = if (expand == internalExpand) {
+                if (expand) Int.MAX_VALUE else limitedMaxLines
+            } else {
+                displayedLines
+            },
+        )
     }
 }
 
@@ -246,8 +204,7 @@ private fun TextLayoutResult.resolveCollapsedText(
     return if (lastCharacterIndex == originalText.length) {
         AnnotatedString(originalText)
     } else {
-        var lastCharIndex =
-            getLineEnd(lineIndex = lastLine, visibleEnd = true) + 1
+        var lastCharIndex = getLineEnd(lineIndex = lastLine, visibleEnd = true) + 1
         var charRect: Rect
         when (getParagraphDirection(lastCharIndex - 1)) {
             Ltr -> {
@@ -282,55 +239,6 @@ private data class ExpandableTextHeights(
     val collapsedHeight: Float,
     val expandedHeight: Float,
 )
-
-private fun SubcomposeMeasureScope.measureExpandableText(
-    expandAction: @Composable (Modifier) -> Unit,
-    collapsedText: @Composable (Modifier, Placeable) -> Unit,
-    expandedText: @Composable (Modifier, Placeable) -> Unit,
-    constraints: Constraints,
-): ExpandableTextHeights {
-    val expandActionPlaceable =
-        subcompose(slotId = "ExpandActionSlot") {
-            expandAction(Modifier.layoutId("ExpandAction"))
-        }
-            .firstOrNull { it.layoutId == "ExpandAction" }
-            ?.measure(Constraints())
-            ?: throw IllegalArgumentException("expandAction needs to apply the provided Modifier")
-    val measurables = subcompose(slotId = "MeasuringSlot") {
-        collapsedText(Modifier.layoutId("CollapsedText"), expandActionPlaceable)
-        expandedText(Modifier.layoutId("ExpandedText"), expandActionPlaceable)
-    }
-    val collapsedHeight = measurables
-        .firstOrNull { it.layoutId == "CollapsedText" }
-        ?.measure(constraints)?.height?.toFloat()
-        ?: throw IllegalArgumentException("collapsedText needs to apply the provided Modifier")
-    val expandedHeight = measurables
-        .firstOrNull { it.layoutId == "ExpandedText" }
-        ?.measure(constraints)?.height?.toFloat()
-        ?: throw IllegalArgumentException("expandedText needs to apply the provided Modifier")
-
-    return ExpandableTextHeights(
-        collapsedHeight = collapsedHeight,
-        expandedHeight = expandedHeight,
-    )
-}
-
-private fun SubcomposeMeasureScope.layoutExpandableText(
-    constraints: Constraints,
-    content: @Composable (Modifier) -> Unit,
-): MeasureResult {
-
-    val placeable = subcompose(slotId = "DisplayedSlot") {
-        content(Modifier.layoutId("DisplayedText"))
-    }
-        .firstOrNull { it.layoutId == "DisplayedText" }
-        ?.measure(constraints)
-        ?: throw IllegalArgumentException("content needs to apply the supplied Modifier")
-
-    return layout(placeable.width, placeable.height) {
-        placeable.place(0, 0)
-    }
-}
 
 @Preview(showBackground = true, heightDp = 700, backgroundColor = 0xffffff)
 @Composable
